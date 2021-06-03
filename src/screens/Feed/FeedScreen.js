3190486571e3aca30359615b952bbbfa4cfb0a32
AbirMazumder99 <abirmazumder1999@gmail.com>
2021-06-03 18:43:40 -0400

    Added code for Swiper/Carousel

--- src/screens/Feed/FeedScreen.js
+++ src/screens/Feed/FeedScreen.js
@@ -1,94 +1,92 @@
 import React from "react";
-import { Image, Text, View, FlatList, SafeAreaView } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import moment from "moment";
+import {
+  Image,
+  Text,
+  View,
+  FlatList,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
 import styles from "./styles";
+import Carousel from "react-native-snap-carousel";
+import SliderEntry from "react-native-snap-carousel/src/carousel/";
 
+const { width: viewportWidth, height: viewportHeight } =
+  Dimensions.get("window");
+
+const slideWidth = wp(75);
+const itemHorizontalMargin = wp(2);
+
+const sliderWidth = viewportWidth;
+const itemWidth = slideWidth + itemHorizontalMargin * 2;
 //Dummy
-const POSTS = [
+const ENTRIES = [
   {
-    id: "1",
-    name: "Joe McKay",
-    text: "oquqweqwe",
-    timestamp: 1569109273726,
-    avatar: require("../../../assets/favicon.png"),
-    image: require("../../../assets/favicon.png"),
+    title: "Favourites landscapes 1",
+    subtitle: "Lorem ipsum dolor sit amet",
+    illustration: "https://i.imgur.com/SsJmZ9jl.jpg",
   },
   {
-    id: "2",
-    name: "Karyn McKay",
-    text: "vnbnvmnbm",
-    timestamp: 1569109273726,
-    avatar: require("../../../assets/favicon.png"),
-    image: require("../../../assets/favicon.png"),
+    title: "Favourites landscapes 2",
+    subtitle: "Lorem ipsum dolor sit amet et nuncat mergitur",
+    illustration: "https://i.imgur.com/5tj6S7Ol.jpg",
   },
   {
-    id: "3",
-    name: "Emer McKay",
-    text: "gdfhdggfjhf",
-    timestamp: 1569109273726,
-    avatar: require("../../../assets/favicon.png"),
-    image: require("../../../assets/favicon.png"),
+    title: "Favourites landscapes 3",
+    subtitle: "Lorem ipsum dolor sit amet et nuncat",
+    illustration: "https://i.imgur.com/pmSqIFZl.jpg",
+  },
+  {
+    title: "Favourites landscapes 4",
+    subtitle: "Lorem ipsum dolor sit amet et nuncat mergitur",
+    illustration: "https://i.imgur.com/cA8zoGel.jpg",
+  },
+  {
+    title: "Favourites landscapes 5",
+    subtitle: "Lorem ipsum dolor sit amet",
+    illustration: "https://i.imgur.com/pewusMzl.jpg",
+  },
+  {
+    title: "Favourites landscapes 6",
+    subtitle: "Lorem ipsum dolor sit amet et nuncat",
+    illustration: "https://i.imgur.com/l49aYS3l.jpg",
   },
 ];
 
 export default function FeedScreen() {
-  const renderPost = ({ item }) => {
-    return (
-      <View style={styles.feedItem}>
-        <Image
-          source={require("../../../assets/abir.jpg")}
-          style={styles.avatar}
-        />
-        <View style={{ flex: 1 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <View>
-              <Text style={styles.name}>{item.name}</Text>
-              <Text style={styles.timestamp}>
-                {moment(item.timestamp).fromNow()}
-              </Text>
-            </View>
-
-            <Ionicons name="ios-more" size={24} color="#73788B" />
-          </View>
-
-          <Text style={styles.post}>{item.text}</Text>
-
-          <Image
-            source={item.image}
-            style={styles.postImage}
-            resizeMode="cover"
-          />
-
-          <View style={{ flexDirection: "row" }}>
-            <Ionicons
-              name="ios-heart-empty"
-              size={24}
-              color="#73788B"
-              style={{ marginRight: 16 }}
-            />
-            <Ionicons name="ios-chatboxes" size={24} color="#73788B" />
-          </View>
-        </View>
-      </View>
-    );
+  const _renderItem = ({ item, index }) => {
+    return <SliderEntry data={item} even={false} />;
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        style={styles.feed}
-        data={POSTS}
-        renderItem={renderPost}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-      ></FlatList>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <StatusBar
+          translucent={true}
+          backgroundColor={"rgba(0, 0, 0, 0.3)"}
+          barStyle={"light-content"}
+        />
+        <ScrollView
+          style={styles.scrollview}
+          scrollEventThrottle={200}
+          directionalLockEnabled={true}
+        >
+          <View style={styles.exampleContainer}>
+            <Text style={styles.title}>Michelle</Text>
+            <Text style={styles.subtitle}>Student at USC</Text>
+            <Carousel
+              data={ENTRIES}
+              renderItem={_renderItem}
+              sliderWidth={sliderWidth}
+              itemWidth={itemWidth}
+              containerCustomStyle={styles.slider}
+              contentContainerCustomStyle={styles.sliderContentContainer}
+              layout="tinder"
+              loop={true}
+            />
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
--- src/screens/Feed/styles.js
+++ src/screens/Feed/styles.js
@@ -1,64 +1,98 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#EFECF4"
-    },
-    header: {
-        paddingTop: 64,
-        paddingBottom: 16,
-        backgroundColor: "#FFF",
-        alignItems: "center",
-        justifyContent: "center",
-        borderBottomWidth: 1,
-        borderBottomColor: "#EBECF4",
-        shadowColor: "#454D65",
-        shadowOffset: { height: 5 },
-        shadowRadius: 15,
-        shadowOpacity: 0.2,
-        zIndex: 10
-    },
-    headerTitle: {
-        fontSize: 20,
-        fontWeight: "500"
-    },
-    feed: {
-        marginHorizontal: 16
-    },
-    feedItem: {
-        backgroundColor: "#FFF",
-        borderRadius: 5,
-        padding: 8,
-        flexDirection: "row",
-        marginVertical: 8
-    },
-    avatar: {
-        width: 36,
-        height: 36,
-        borderRadius: 18,
-        marginRight: 16
-    },
-    name: {
-        fontSize: 15,
-        fontWeight: "500",
-        color: "#454D65"
-    },
-    timestamp: {
-        fontSize: 11,
-        color: "#C4C6CE",
-        marginTop: 4
-    },
-    post: {
-        marginTop: 16,
-        fontSize: 14,
-        color: "#838899"
-    },
-    postImage: {
-        width: undefined,
-        height: 150,
-        borderRadius: 5,
-        marginVertical: 16
-    }
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#EFECF4",
+  },
+  exampleContainer: {
+    paddingVertical: 30,
+    backgroundColor: "black",
+  },
+  title: {
+    paddingHorizontal: 30,
+    backgroundColor: "transparent",
+    color: "rgba(255, 255, 255, 0.9)",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subtitle: {
+    marginTop: 5,
+    paddingHorizontal: 30,
+    backgroundColor: "transparent",
+    color: "rgba(255, 255, 255, 0.75)",
+    fontSize: 13,
+    fontStyle: "italic",
+    textAlign: "center",
+  },
+  slider: {
+    marginTop: 15,
+    overflow: "visible", // for custom animations
+  },
+  sliderContentContainer: {
+    paddingVertical: 10, // for custom animation
+  },
+  scrollview: {
+    flex: 1,
+  },
+  header: {
+    paddingTop: 64,
+    paddingBottom: 16,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#EBECF4",
+    shadowColor: "#454D65",
+    shadowOffset: { height: 5 },
+    shadowRadius: 15,
+    shadowOpacity: 0.2,
+    zIndex: 10,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  feed: {
+    marginHorizontal: 16,
+  },
+  feedItem: {
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    padding: 8,
+    flexDirection: "row",
+    marginVertical: 8,
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 16,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#454D65",
+  },
+  timestamp: {
+    fontSize: 11,
+    color: "#C4C6CE",
+    marginTop: 4,
+  },
+  post: {
+    marginTop: 16,
+    fontSize: 14,
+    color: "#838899",
+  },
+  postImage: {
+    width: undefined,
+    height: 150,
+    borderRadius: 5,
+    marginVertical: 16,
+  },
 });
